df3bb7df50d50eef93f5dc5051214dc3c444cae8
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-10-20 22:55:15 +0530

    Added UC3

---
+++ src/_UC3_OptionCheckSnL.java
@@ -0,0 +1,40 @@
+public class _UC3_OptionCheckSnL {
+	int dice() {
+		int dice=(int)(Math.random()*10%6+1);
+		return dice;
+	}
+	
+	int option() {
+		int option=(int)(Math.random()*10%3+1);
+		return option;
+			
+		}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		int position=0;
+		_UC3_OptionCheckSnL rv=new _UC3_OptionCheckSnL();
+		System.out.println("Welcome To Snake And Ladder Game");
+		System.out.println("Player 1 is at position "+position);
+		System.out.println("Player 1 rolls the dice and got");
+		System.out.println(rv.dice());
+		int option=rv.option();
+		if(option==1) {
+			System.out.println("NO PLAY | Player stay at position where he was");
+		}
+		else if (option==2) {
+			System.out.println("LADDER |Player Move ahead by Number "+rv.dice());
+			position=position+rv.dice();
+		}
+		else if (option==3) {
+			System.out.println("SNAKE |Player Bitten by snake go back by Number  "+rv.dice());
+			position=position-rv.dice();
+		}
+		
+	}
+
+}
