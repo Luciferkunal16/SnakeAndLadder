1e3a1c29f8145ee1195868691cf3c36292782028
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-10-20 22:39:56 +0530

    UC2 is Added

--- src/_UC2_PlayerRollsSnL.java
+++ src/_UC2_PlayerRollsSnL.java
@@ -1,14 +1,20 @@
 
 public class _UC2_PlayerRollsSnL {
+	int dice() {
+		int dice=(int)(Math.random()*10%6+1);
+		return dice;
+	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		int position=0;
+		_UC2_PlayerRollsSnL rv=new _UC2_PlayerRollsSnL();
 		System.out.println("Welcome To Snake And Ladder Game");
 		System.out.println("Player 1 is at position "+position);
-		int dice=(int)(Math.random()*10%6+1);
 		System.out.println("Player 1 rolls the dice and got");
-		System.out.println(dice);
+		System.out.println(rv.dice());
 			
 		
 	}
-
-}
